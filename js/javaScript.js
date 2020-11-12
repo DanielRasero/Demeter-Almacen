75cd23d7645516c14a80b1101ef7b94390f5ce14
dani <danielraseromarin@gmail.com>
2020-11-12 12:34:22 +0100

    interfaz completada

---
+++ js/javaScript.js
@@ -0,0 +1,32 @@
+
+let objetosDemeter={
+
+    "objetos":[
+
+        {"nombre":"Objeto1","tipo":"","caracteristicas":"","fotos":"","precio":"","web":""},
+        {"nombre":"Objeto2","tipo":"","caracteristicas":"","fotos":"","precio":"","web":""},
+        {"nombre":"Objeto3","tipo":"","caracteristicas":"","fotos":"","precio":"","web":""},
+        {"nombre":"Objeto4","tipo":"","caracteristicas":"","fotos":"","precio":"","web":""},
+        {"nombre":"Objeto5","tipo":"","caracteristicas":"","fotos":"","precio":"","web":""},
+        {"nombre":"Objeto6","tipo":"","caracteristicas":"","fotos":"","precio":"","web":""},
+        {"nombre":"Objeto7","tipo":"","caracteristicas":"","fotos":"","precio":"","web":""},
+        {"nombre":"Objeto8","tipo":"","caracteristicas":"","fotos":"","precio":"","web":""},
+        {"nombre":"Objeto9","tipo":"","caracteristicas":"","fotos":"","precio":"","web":""},
+        {"nombre":"","tipo":"","caracteristicas":"","fotos":"","precio":"","web":""},
+        {"nombre":"","tipo":"","caracteristicas":"","fotos":"","precio":"","web":""},
+        {"nombre":"","tipo":"","caracteristicas":"","fotos":"","precio":"","web":""},
+        {"nombre":"","tipo":"","caracteristicas":"","fotos":"","precio":"","web":""}
+    ]
+};
+
+
+for (let i = 0; i < 10; i++) {
+
+    let fila="<div class='row-sm'>";
+
+    fila+="<div id="+i+" class='col-sm'>"+objetosDemeter.objetos[i].nombre+"</div>";
+
+    fila+= "</div>";
+    document.getElementById("cuadritos").innerHTML+= fila;
+}
+
