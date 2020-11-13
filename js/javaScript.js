821d8d63a2f802b8ee5b33094b49a4b1d0f624e4
dani <danielraseromarin@gmail.com>
2020-11-13 22:55:34 +0100

    Finalización de visión en modo lista

--- js/javaScript.js
+++ js/javaScript.js
@@ -36,14 +36,35 @@ function listado() {
     var objeto= document.getElementById("cuadritos");
     objeto.innerHTML="";
 
+    let fila="<ul class='list-group'>";
+
+    for (let i = 0; i < 10; i++) {
+
+        fila+="<li class='list-group-item'> " +
+            "<div class='img-lista'>"+objetosDemeter.objetos[i].fotos+"</div>" +
+            "<div class='nombre-lista'>"+objetosDemeter.objetos[i].nombre+" </div>" +
+            "</li>";
+    }
+
+    fila+= "</div>";
+    document.getElementById("cuadritos").innerHTML+= fila;
+
+}
+
+
+function grideado() {
+
+    var objeto= document.getElementById("cuadritos");
+    objeto.innerHTML="";
+
     for (let i = 0; i < 10; i++) {
 
         let fila="<div class='row-sm'>";
 
-        fila+="<div id="+i+" class='col-sm' id='col-lista'>"+objetosDemeter.objetos[i].fotos+"</div>";
-        fila+="<div id="+i+"'colum' class='col-7'>"+objetosDemeter.objetos[i].nombre+"</div>";
+        fila+="<div id="+i+" class='col-sm'>"+objetosDemeter.objetos[i].nombre+"</div>";
 
         fila+= "</div>";
         document.getElementById("cuadritos").innerHTML+= fila;
     }
+
 }
